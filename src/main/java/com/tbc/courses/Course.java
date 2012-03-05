ed0fe6c3cd098558e640480cb0d24c88c44d8d91
Stefan Wehner <stefan.wehner@gmail.com>
2012-03-05 20:03:18 +0100

    Added course and teacher relationship. Testing file database

---
+++ src/main/java/com/tbc/courses/Course.java
@@ -0,0 +1,59 @@
+package com.tbc.courses;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Course {
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+	private Long id;
+	private String name;
+	@ManyToOne
+	@JoinColumn(name = "teacher_id")
+	private Teacher teacher;
+
+	public Course(){
+		
+	}
+	
+	public Course(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Teacher getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@SuppressWarnings("unused")
+	private void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + "]";
+	}
+}
